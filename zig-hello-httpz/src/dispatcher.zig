7fd869b6aff4a155277009f9a65ef41a08a704e2
dxps <marius.ileana@gmail.com>
2023-09-20 20:50:37 +0300

    chore(zig-hello-httpz): adapt to httpz upd

--- zig-hello-httpz/src/dispatcher.zig
+++ zig-hello-httpz/src/dispatcher.zig
@@ -2,43 +2,45 @@ const std = @import("std");
 const httpz = @import("httpz");
 const Allocator = std.mem.Allocator;
 
-/// The global state.
+// Our global state (just like global.zig)
 const GlobalContext = struct {
     hits: usize = 0,
     l: std.Thread.Mutex = .{},
 };
 
-/// Per-request data.
+// our per-request data
 const RequestContext = struct {
+    const Self = @This();
+
     user_id: ?[]const u8,
     global: *GlobalContext,
+
+    pub fn increment(self: *Self, _: *httpz.Request, res: *httpz.Response) !void {
+        // we don't actually do anything with ctx.user_id
+        // except make sure it's been set. This could be common in a route
+        // where any user can take an action as long as they're logged in.
+
+        if (self.user_id == null) return notAuthorized(res);
+
+        self.global.l.lock();
+        var hits = self.global.hits + 1;
+        self.global.hits = hits;
+        self.global.l.unlock();
+
+        res.content_type = httpz.ContentType.TEXT;
+        var out = try std.fmt.allocPrint(res.arena, "{d} hits", .{hits});
+        res.body = out;
+    }
 };
 
 pub fn start(allocator: Allocator) !void {
     var ctx = GlobalContext{};
-    var server = try httpz.ServerCtx(*GlobalContext, *RequestContext)
-        .init(allocator, .{ .pool_size = 10, .port = 5884 }, &ctx);
+    var server = try httpz.ServerCtx(*GlobalContext, *RequestContext).init(allocator, .{ .pool = .{ .min = 20 }, .port = 5884 }, &ctx);
+    defer server.deinit();
     server.dispatcher(dispatcher);
     var router = server.router();
-    router.get("/increment", increment);
+    router.get("/increment", RequestContext.increment);
     return server.listen();
-}
-
-fn increment(_: *httpz.Request, res: *httpz.Response, ctx: *RequestContext) !void {
-    // we don't actually do anything with ctx.user_id
-    // except make sure it's been set. This could be common in a route
-    // where any user can take an action as long as they're logged in.
-
-    if (ctx.user_id == null) return notAuthorized(res);
-
-    ctx.global.l.lock();
-    var hits = ctx.global.hits + 1;
-    ctx.global.hits = hits;
-    ctx.global.l.unlock();
-
-    res.content_type = httpz.ContentType.TEXT;
-    var out = try std.fmt.allocPrint(res.arena, "{d} hits", .{hits});
-    res.body = out;
 }
 
 fn notAuthorized(res: *httpz.Response) void {
@@ -46,7 +48,7 @@ fn notAuthorized(res: *httpz.Response) void {
     res.body = "Not authorized";
 }
 
-fn dispatcher(action: httpz.Action(*RequestContext), req: *httpz.Request, res: *httpz.Response, global: *GlobalContext) !void {
+fn dispatcher(global: *GlobalContext, action: httpz.Action(*RequestContext), req: *httpz.Request, res: *httpz.Response) !void {
     // If we you need to allocate memory here, consider using req.arena
 
     // this is obviously a dummy example where we just trust the "user" header
@@ -54,5 +56,5 @@ fn dispatcher(action: httpz.Action(*RequestContext), req: *httpz.Request, res: *
         .global = global,
         .user_id = req.header("user"),
     };
-    return action(req, res, &ctx);
+    return action(&ctx, req, res);
 }
