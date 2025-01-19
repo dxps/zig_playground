46a17f663954d7109508d31d37205f1b8959854d
dxps <marius.ileana@gmail.com>
2025-01-19 14:29:31 +0200

    codecrafters-http-server-zig: respond with body (doc upd)

--- codecrafters-http-server-zig/readme.md
+++ codecrafters-http-server-zig/readme.md
@@ -1,7 +1,8 @@
-[![progress-banner](https://backend.codecrafters.io/progress/http-server/28494212-4c3e-44cf-9cff-c613ef267821)](https://app.codecrafters.io/users/codecrafters-bot?r=2qF)
+# Build Your Own HTTP server in Zig
 
-This is a starting point for Zig solutions to the
-["Build Your Own HTTP server" Challenge](https://app.codecrafters.io/courses/http-server/overview).
+This the CodeCrafters' challenge of [Build Your Own HTTP server](https://app.codecrafters.io/courses/http-server/overview) in [Zig](https://ziglang.org/) programming language.
+
+[![progress-banner](https://backend.codecrafters.io/progress/http-server/28494212-4c3e-44cf-9cff-c613ef267821)](https://app.codecrafters.io/users/codecrafters-bot?r=2qF)
 
 [HTTP](https://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol) is the
 protocol that powers the web. In this challenge, you'll build a HTTP/1.1 server
@@ -11,11 +12,19 @@ Along the way you'll learn about TCP servers,
 [HTTP request syntax](https://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html),
 and more.
 
-**Note**: If you're viewing this repo on GitHub, head over to
-[codecrafters.io](https://codecrafters.io) to try the challenge.
+<br/>
 
----
+## Setup
 
 -   Ensure you have `zig (0.14)` installed locally
 -   Run `./your_program.sh` to run your program, which is implemented in
     `src/main.zig`.
+
+<br/>
+
+## Usage
+
+```bash
+curl -i http://localhost:4221/ # must respond with OK
+curl -i http://localhost:4221/echo/abc # must respond with OK and body: abc
+```
--- codecrafters-http-server-zig/src/main.zig
+++ codecrafters-http-server-zig/src/main.zig
@@ -11,16 +11,47 @@ pub fn main() !void {
 
     printout("Listening to 127.0.0.1:4221 ...\n", .{});
 
-    var conn = try listener.accept();
-
+    const conn = try listener.accept();
     printout("Received connection from {}.\n", .{conn.address});
+    const stream = conn.stream;
 
-    // Explicitly handle the potential error.
-    _ = conn.stream.write("HTTP/1.1 200 OK\r\n\r\n") catch |err| {
-        std.log.err("Failed to write to connection: {}", .{err});
+    // Allocate a buffer to read into.
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const a = gpa.allocator();
+    const buff = try a.alloc(u8, 512);
+    defer a.free(buff);
+
+    _ = stream.read(buff) catch |err| {
+        printout("Failed to read from client stream: {}\n", .{err});
     };
-    conn.stream.close();
 
-    // This is the concise version (at least minimal enough for the current case).
-    // try conn.stream.writeAll("HTTP/1.1 200 OK\r\n\r\n");
+    var iter = std.mem.splitAny(u8, buff, " ");
+    _ = iter.next(); // skip the HTTP method (first word in the first line)
+    const url_path = iter.next();
+
+    if (std.mem.eql(u8, url_path.?, "/")) {
+        try respond_ok(stream);
+        return;
+    }
+
+    if (url_path.?.len < 6) {
+        try respond_not_found(stream);
+        return;
+    }
+
+    const path = url_path.?[6..]; // the text in "/echo/{path}"
+    if (std.mem.eql(u8, url_path.?[0..6], "/echo/")) {
+        const res = try std.fmt.allocPrint(a, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {d}\r\n\r\n{s}", .{ path.len, path });
+        try stream.writeAll(res);
+    } else {
+        try respond_not_found(stream);
+    }
+}
+
+fn respond_ok(stream: anytype) !void {
+    try stream.writeAll("HTTP/1.1 200 OK\r\n\r\n");
+}
+
+fn respond_not_found(stream: anytype) !void {
+    try stream.writeAll("HTTP/1.1 404 Not Found\r\n\r\n");
 }
