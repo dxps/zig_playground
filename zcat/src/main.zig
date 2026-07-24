3da4fdcb2ae7a337ec07ead2fcdbdf45a21f434d
dxps <marius.ileana@gmail.com>
2026-07-24 13:08:42 +0300

    zcat: use streaming for reading files and writing to stdout

--- zcat/src/main.zig
+++ zcat/src/main.zig
@@ -1,39 +1,38 @@
 const std = @import("std");
-const Io = std.Io;
 
 const zcat = @import("zcat");
 
-pub fn main(init: std.process.Init) !u8 {
+pub fn main(init: std.process.Init) !void {
+    var stdout_buffer: [1024]u8 = undefined;
+    var stderr_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.Io.File.stdout().writerStreaming(init.io, &stdout_buffer);
+    var stderr_writer = std.Io.File.stderr().writerStreaming(init.io, &stderr_buffer);
+    const stdout = &stdout_writer.interface;
+    const stderr = &stderr_writer.interface;
+
     var gpa = std.heap.DebugAllocator(.{}).init;
     defer _ = gpa.deinit();
     const ally = gpa.allocator();
 
-    const cwd = std.Io.Dir.cwd();
-
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.Io.File.stdout().writer(init.io, &stdout_buffer);
-    const stdout = &stdout_writer.interface;
-
     const args = try init.minimal.args.toSlice(ally);
     defer ally.free(args);
 
-    for (args[1..]) |filepath| {
-        const max_bytes = 16 * 1024 * 1024;
-        const text = cwd.readFileAlloc(init.io, filepath, ally, .limited(max_bytes)) catch |err| switch (err) {
-            error.FileNotFound => {
-                std.debug.print("Error: file not found '{s}'.\n", .{filepath});
-                continue;
-            },
-            else => {
-                std.debug.print("Error: failed reading file: {s}: {s}\n", .{ filepath, @errorName(err) });
-                continue;
-            },
-        };
-        defer ally.free(text);
+    const cwd = std.Io.Dir.cwd();
 
-        try stdout.writeAll(text);
+    for (args[1..]) |filepath| {
+        const file = cwd.openFile(init.io, filepath, .{}) catch {
+            stderr.print("Error: could not open file '{s}'.\n", .{filepath}) catch {};
+            stderr.flush() catch {};
+            continue;
+        };
+        defer file.close(init.io);
+
+        var file_buffer: [1024]u8 = undefined;
+        var file_reader = file.reader(init.io, &file_buffer);
+        const reader = &file_reader.interface;
+
+        _ = try reader.streamRemaining(stdout);
     }
 
     try stdout.flush();
-    return 0;
 }
